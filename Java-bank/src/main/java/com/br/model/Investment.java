0caf67f955453ae8c8eb3b080172a38b654afa3d
Anderson Freires <dre.son36@gmail.com>
2025-08-13 09:26:32 -0300

    "Class" Investment with record for saving values

---
+++ Java-bank/src/main/java/com/br/model/Investment.java
@@ -0,0 +1,19 @@
+package com.br.model;
+
+
+public record Investment(
+        long id,
+        long tax,
+        long initialFunds)
+{
+	
+	
+    @Override
+    public String toString() {
+        return "Investment{" +
+                "id=" + id +
+                ", tax=" + tax + "%" +
+                ", initialFunds=" + (initialFunds / 100) + "," + (initialFunds % 100) +
+                '}';
+    }
+}
