9a192d502c1b88b0a85b577944461aa590306cfe
Anderson Freires <dre.son36@gmail.com>
2025-08-13 08:55:23 -0300

    Creating wallet account class

--- Java-bank/src/main/java/com/br/model/Money.java
+++ Java-bank/src/main/java/com/br/model/Money.java
@@ -1,5 +1,9 @@
 package com.br.model;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.ToString;
@@ -9,5 +13,20 @@ import lombok.ToString;
 @Getter
 
 public class Money {
+			private final List<MoneyAudit> history= new ArrayList<>();
+
+			public Money(final MoneyAudit history) {
+				this.history.add(history);
+			}
+			
+			public void addHistory(final MoneyAudit history) {
+				this.history.add(history);
+			}
+
+			public Collection<Money> getHistory() {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			
 			
 }
--- Java-bank/src/main/java/com/br/model/Wallet.java
+++ Java-bank/src/main/java/com/br/model/Wallet.java
@@ -1,35 +1,57 @@
 package com.br.model;
 
+import lombok.Getter;
+
+import java.time.OffsetDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
+import java.util.stream.Stream;
 
-import lombok.Getter;
-import lombok.ToString;
-
-@ToString
 public abstract class Wallet {
 
-	@Getter
-	private final BankingService bankingService;
-	
-	
-	protected final List<Money> moneys;
+    @Getter
+    private final BankingService service;
 
+    protected final List<Money> money;
 
-	public Wallet(final BankingService bankingService) {
-		super();
-		this.bankingService = bankingService;
-		this.moneys=new ArrayList<>();
-	}
-	
-	protected List<Money> generateMoney(final long amount,final String description){
-		
-	}
-	
-	public long getFunds() {
-		return moneys.size();
-	}
-	
-	
-	
+    public Wallet(final BankingService serviceType) {
+        this.service = serviceType;
+        this.money = new ArrayList<>();
+    }
+
+    protected List<Money> generateMoney(final long amount, final String description){
+        var history = new MoneyAudit(UUID.randomUUID(), service, description, OffsetDateTime.now());
+        return Stream.generate(() -> new Money(history)).limit(amount).toList();
+    }
+
+    public long getFunds(){
+        return money.size();
+    }
+
+    public void addMoney(final List<Money> money, final BankingService service, final String description){
+        var history = new MoneyAudit(UUID.randomUUID(), service, description, OffsetDateTime.now());
+        money.forEach(m -> m.addHistory(history));
+        this.money.addAll(money);
+    }
+
+    public List<Money> reduceMoney(final long amount){
+        List<Money> toRemove = new ArrayList<>();
+        for (int i = 0; i < amount; i++) {
+            toRemove.add(this.money.removeFirst());
+        }
+        return toRemove;
+    }
+
+    public List<MoneyAudit> getFinancialTransactions(){
+        return money.stream().flatMap(m -> m.getHistory().stream()).toList();
+    }
+
+    @Override
+    public String toString() {
+        return "Wallet{" +
+                "service=" + service +
+                ", money= R$" + money.size() / 100 + "," + money.size() % 100 +
+                '}';
+    }
 }
