193217171518eb9b0fbe3bd317503dce77be0fd3
Anderson Freires <dre.son36@gmail.com>
2025-08-13 11:05:24 -0300

    Custom unchecked exception to handle a specific business rule: when a PIX key is already in use.

--- Java-bank/src/main/java/com/br/exception/PixInUseException.java
+++ Java-bank/src/main/java/com/br/exception/PixInUseException.java
@@ -1,5 +1,10 @@
 package com.br.exception;
 
-public class PixInUseException {
+
+public class PixInUseException extends RuntimeException {
+
+    public PixInUseException(String message) {
+        super(message);
+    }
 
 }
