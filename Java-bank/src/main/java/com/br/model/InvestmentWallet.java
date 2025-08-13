e36c9b0c23c83f36cd182efa2e36a3fe09a8af64
Anderson Freires <dre.son36@gmail.com>
2025-08-13 10:39:55 -0300

    investment portfolio that inherits from Wallet and manages both the initial invested amount and the return over time.

---
+++ Java-bank/src/main/java/com/br/model/InvestmentWallet.java
@@ -0,0 +1,30 @@
+package com.br.model;
+
+@Getter
+public class InvestmentWallet extends Wallet{
+
+    private final Investment investment;
+    private final AccountWallet account;
+
+    public InvestmentWallet(final Investment investment, final AccountWallet account, final long amount) {
+        super(INVESTMENT);
+        this.investment = investment;
+        this.account = account;
+        addMoney(account.reduceMoney(amount), getService(), "investimento");
+    }
+
+    public void updateAmount(final long percent){
+        var amount = getFunds() * percent / 100;
+        var history = new MoneyAudit(UUID.randomUUID(), getService(), "rendimentos", OffsetDateTime.now());
+        var money = Stream.generate(() -> new Money(history)).limit(amount).toList();
+        this.money.addAll(money);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "InvestmentWallet{" +
+                "investment=" + investment +
+                ", account=" + account +
+                '}';
+    }
+}
